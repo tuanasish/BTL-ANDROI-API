410118c53012fe07f26608fea99bed306f1383a4
Nguyen Minh Duc <nguyenminhduc240204@gmail.com>
2025-03-22 21:03:09 +0700

    api

---
+++ app/src/main/java/com/example/btl/api/ApiTimeSlotClient.java
@@ -0,0 +1,31 @@
+package com.example.btl.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiTimeSlotClient {
+    public static final String BASE_URL = "http://localhost:6000/v1/";
+
+    private static Retrofit retrofit = null;
+    public static  Retrofit getClient(){
+        if (retrofit == null) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(loggingInterceptor)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
+
